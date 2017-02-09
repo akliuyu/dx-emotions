26d0c65fc7f50755153010ce5f38ca83e8207d2f
huangjue <huangjue@meituan.com>
2017-02-09 21:22:06 +0800

    完成导入功能 导出功能目前只能打到控制台中

--- js/options.js
+++ js/options.js
@@ -1,30 +1,75 @@
 /**
- * 
+ * @module 表情插件管理模块
  */
-Array.prototype.remove = function(item) {
-    var index = this.indexOf(item);
-    var re = this[index];
+Array.prototype.remove = function (item) {
+    let index = this.indexOf(item);
+    let re = this[index];
     if (index >= 0) {
         this.splice(index, 1);
     }
     return re;
+};
+
+function saveFavor(favorList) {
+    chrome.storage.local.set({
+        'favorList': favorList,
+        'hasNew': true
+    }, function () {
+
+    });
 }
 
-var myApp = angular.module('myApp', []);
+let myApp = angular.module('myApp', []);
 
-myApp.controller('OptionCtrl', [ '$scope', function($scope) {
+myApp.controller('OptionCtrl', ['$scope', function ($scope) {
     $scope.favors = [];
-    chrome.storage.local.get('favorList', function(items) {
-        $scope.$apply(function() {
-            var favorList = items['favorList'];
+    chrome.storage.local.get('favorList', function (items) {
+        $scope.$apply(function () {
+            let favorList = items['favorList'];
             $scope.favors = favorList;
         });
     });
-    $scope.remove = function(url) {
+    $scope.remove = function (url) {
         $scope.favors.remove(url);
         chrome.storage.local.set({
-            'favorList' : $scope.favors,
-            'hasNew' : true
+            'favorList': $scope.favors,
+            'hasNew': true
         });
     };
-} ]);
+
+    $scope.importEmotions = function (emotions) {
+        try {
+            let emotionsArr = JSON.parse(emotions);
+            chrome.storage.local.get('favorList', (items)=> {
+                let favorList = items['favorList'];
+                let result = [];
+                if (favorList) {
+                    const hashMap = new Map();
+                    favorList.forEach((emotion)=> {
+                        hashMap.set(emotion, emotion);
+                    });
+
+                    emotionsArr.forEach((emotion)=> {
+                        if (!hashMap.has(emotion)) {
+                            hashMap.set(emotion, emotion);
+                        }
+                    });
+
+                    for (let [key, value] of hashMap) {
+                        result.push(value);
+                    }
+
+                    saveFavor(result);
+                    location.reload();
+                }
+            });
+        } catch (e) {
+            alert('请输入正确的导入数据，或重新检查下~~');
+        }
+    };
+
+    $scope.exportEmotions = function () {
+        console.debug(JSON.stringify($scope.favors));
+    };
+}]);
+
